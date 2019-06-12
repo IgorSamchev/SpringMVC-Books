c638daed0f8ef7ea66802c22b7a01c7fc79f5f06
Igor.Samchev <Igor.samchev@gmail.com>
2019-06-12 09:26:16 +0300

    JS Add and Edit field checker script join

--- src/main/resources/static/js/appJS.js
+++ src/main/resources/static/js/appJS.js
@@ -1,8 +1,8 @@
-function addNewBook(language) {
-
+function fieldChecker(id, language) {
     let title = 'AddedBookTitle=' + document.getElementById("titleID").value;
     let author = 'AddedBookAuthor=' + document.getElementById("authorID").value;
     let isbn = 'AddedBookISBN=' + document.getElementById("isbnID").value;
+    let ref = id + title + author + isbn;
 
     if (title.length > 17 && title.length < 56) {
         document.getElementById('titleError').innerText = "";
@@ -10,6 +10,7 @@ function addNewBook(language) {
             document.getElementById('authorError').innerText = "";
             if (isbn.length > 16 && isbn.length < 29) {
                 document.getElementById('isbnError').innerText = "";
+                return document.location.href = "/edit_book/" + ref;
             } else {
                 if (language.includes('en'))
                     document.getElementById('isbnError').innerText
@@ -41,53 +42,6 @@ function addNewBook(language) {
                 = "Pealkiri peaks olema vähemalt 3 ja maksimaalselt 30 tähemärki";
         else if (language === 'ru')
             document.getElementById('titleError').innerText
-                = "Поле Наименование должно быть минимумально 3 и максимально 30 символов";
-    }
-}
-
-function editBook(id, language) {
-    let title = 'AddedBookTitle=' + document.getElementById("titleEdit").value;
-    let author = 'AddedBookAuthor=' + document.getElementById("authorEdit").value;
-    let isbn = 'AddedBookISBN=' + document.getElementById("isbnEdit").value;
-    let ref = id + title + author + isbn;
-    if (title.length > 17 && title.length < 56) {
-        document.getElementById('titleEditError').innerText = "";
-        if (author.length > 18 && author.length < 37) {
-            document.getElementById('AuthorEditError').innerText = "";
-            if (isbn.length > 16 && isbn.length < 29) {
-                document.getElementById('isbnEditError').innerText = "";
-                return document.location.href = "/edit_book/" + ref;
-            } else {
-                if (language.includes('en'))
-                    document.getElementById('isbnEditError').innerText
-                        = "ISBN should be a minimum of 3 and maximum of 13 characters";
-                else if (language.includes('ee'))
-                    document.getElementById('isbnEditError').innerText
-                        = "ISBN peaks olema vähemalt 3 ja maksimaalselt 13 tähemärki";
-                else if (language.includes('ru'))
-                    document.getElementById('isbnEditError').innerText
-                        = "Поле ISBN должно быть минимумально 3 и максимально 13 символов";
-            }
-        } else {
-            if (language.includes('en'))
-                document.getElementById('AuthorEditError').innerText
-                    = "Author should be a minimum of 3 and maximum of 20 characters";
-            else if (language.includes('ee'))
-                document.getElementById('AuthorEditError').innerText
-                    = "Autor peaks olema vähemalt 3 ja maksimaalselt 20 tähemärki";
-            else if (language.includes('ru'))
-                document.getElementById('AuthorEditError').innerText
-                    = "Поле Автор должно быть минимумально 3 и максимально 20 символов";
-        }
-    } else {
-        if (language.includes('en'))
-            document.getElementById('titleEditError').innerText
-                = "Title should be a minimum of 3 and maximum of 30 characters";
-        else if (language.includes('ee'))
-            document.getElementById('titleEditError').innerText
-                = "Pealkiri peaks olema vähemalt 3 ja maksimaalselt 30 tähemärki";
-        else if (language.includes('ru'))
-            document.getElementById('titleEditError').innerText
                 = "Поле Наименование должно быть минимумально 3 и максимально 30 символов";
     }
 }
