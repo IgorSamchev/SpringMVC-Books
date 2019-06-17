7cfefdc41c656bff9b8a129a5b9ee8283952819e
Igor.Samchev <Igor.samchev@gmail.com>
2019-06-17 22:30:56 +0300

    new languages system test drive

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @SpringBootApplication
-public class DemoApplication extends WebMvcConfigurerAdapter {
+public class DemoApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -32,7 +33,7 @@ public class DemoApplication extends WebMvcConfigurerAdapter {
         return localeChangeInterceptor;
     }
 
-    @Override
+
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(localeChangeInterceptor());
     }
